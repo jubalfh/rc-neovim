7bc4b39dddc294c42088fcbcbc99ff06eac7a5e0
Mirosław Baran <miroslaw+c+rcfiles@makabra.org>
2023-10-20 02:49:03 +0200

    (omni)completion and theming changes
    * plugin management
      + nvim-lualine/lualine.nvim,
      + akinsho/bufferline.nvim,
      + hrsh7th/nvim-cmp,
      + hrsh7th/cmp-buffer,
      + hrsh7th/cmp-cmdline,
      + hrsh7th/cmp-nvim-lsp,
      + hrsh7th/cmp-path,
      + hrsh7th/cmp-vsnip,
      + hrsh7th/vim-vsnip,
      - vim-airline/vim-airline,
      - vim-airline/vim-airline-themes,
      - retorillo/airline-tablemode.vim,
    
    [nvim-lualine]
    * approximate airline's layout and vibe
    
    [completion]
    * mucomplete-like setup via nvim-cmp and using nvim native features,
    
    [bikeshedding]
    * splitting config files
    * whitespace fixes

---
+++ .config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,98 @@
+return {
+   'hrsh7th/nvim-cmp',
+   event = "VeryLazy",
+   dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/vim-vsnip',
+      'hrsh7th/cmp-vsnip',
+   },
+   config = function()
+      vim.opt.completeopt = { 'menu', 'menuone', 'noselect', 'noinsert', 'preview', }
+
+      local cmp = require('cmp')
+
+      local has_words_before = function()
+         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local feedkey = function(key, mode)
+         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+      end
+
+      cmp.setup({
+         snippet = {
+            expand = function(args) vim.fn["vsnip#anonymous"](args.body) end,
+         },
+         window = {
+            -- completion = cmp.config.window.bordered(),
+            -- documentation = cmp.config.window.bordered(),
+         },
+         mapping = cmp.mapping.preset.insert({
+            ["<Tab>"] = cmp.mapping(function(fallback)
+               if cmp.visible() then
+                  cmp.select_next_item()
+               elseif vim.fn["vsnip#available"](1) == 1 then
+                  feedkey("<Plug>(vsnip-expand-or-jump)", "")
+               elseif has_words_before() then
+                  cmp.complete()
+               else
+                  fallback()
+                  -- The fallback function sends a already mapped
+                  -- key. In this case, it's probably `<Tab>`.
+               end
+            end, { "i", "s" }),
+
+            ["<S-Tab>"] = cmp.mapping(function()
+               if cmp.visible() then
+                  cmp.select_prev_item()
+               elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                  feedkey("<Plug>(vsnip-jump-prev)", "")
+               end
+            end, { "i", "s" }),
+
+            ["<C-k>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+            ["k"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+
+            ["<C-j>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+            ["j"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+            ["<C-Space>"] = cmp.mapping.complete(),
+
+            ["<C-e>"] = cmp.mapping.abort(),
+
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            -- Accept currently selected item. Set `select` to
+            -- `false` to only confirm explicitly selected items.
+            --
+            ["<S-CR>"] = cmp.mapping.confirm({
+               behavior = cmp.ConfirmBehavior.Replace,
+               select = true,
+            }),
+            -- Accept currently selected item. Set `select` to
+            -- `false` to only confirm explicitly selected items.
+            --
+            ["<C-CR>"] = function(fallback)
+               cmp.abort()
+               fallback()
+            end,
+
+         }),
+         sources = cmp.config.sources({
+            { name = 'ale' },
+            { name = 'nvim_lsp' },
+            { name = 'vsnip' },
+         }, {
+            { name = 'path' },
+            { name = 'buffer' },
+         }),
+      })
+   end
+}
--- .config/nvim/lua/plugins/filetypes.lua
+++ .config/nvim/lua/plugins/filetypes.lua
@@ -1,12 +1,12 @@
 return {
-    -- file formats
-    { 'pearofducks/ansible-vim', lazy = false, },
-    { 'lepture/vim-jinja', lazy = false, },
-    'avakhov/vim-yaml',
-    'elzr/vim-json',
-    'plasticboy/vim-markdown',
-    'jubalfh/vim-ldapschema',
-    'aklt/plantuml-syntax',
-    'nfnty/vim-nftables',
-    'PProvost/vim-ps1',
+  -- file formats
+  { 'pearofducks/ansible-vim', lazy = false, },
+  { 'lepture/vim-jinja', lazy = false, },
+  'avakhov/vim-yaml',
+  'elzr/vim-json',
+  'plasticboy/vim-markdown',
+  'jubalfh/vim-ldapschema',
+  'aklt/plantuml-syntax',
+  'nfnty/vim-nftables',
+  'PProvost/vim-ps1',
 }
